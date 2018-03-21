9af5f96019acb1004d1f07a74f4e9ad58980dd46
Bui Sy Nguyen <nguyenbs@gmail.com>
2018-03-21 12:52:32 +0700

    Upgrade flash player ver 28.0.0

--- getFpFromArchive.sh
+++ getFpFromArchive.sh
@@ -62,8 +62,6 @@ curl -L ${FP_ZIP_ARCHIVE_URL} >> ${FP_ARCHIVE_ZIP}
 echo "[INFO] Unzipping to: ${FP_ARCHIVE_PATH}"
 unzip ${FP_ARCHIVE_ZIP} -d ${FP_ARCHIVE_PATH}
 
-chmod -R +x ${FP_ARCHIVE_PATH}
-
 # locate the mac_sa_debug.app.zip file and ignore any .* files
 # these seem to be inconsistent, "*mac_sa_debug.app.zip" and
 # "*mac_sa_debug.zip" have been notedr,
